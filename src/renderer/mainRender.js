aefbb28985e3819ecc8635f65904cbf4e70f859a
B4YTT73-IDF\Admin <liushuai.798@bytedance.com>
2023-06-30 16:56:42 +0800

    feat: 测试yi

--- src/renderer/mainRender.js
+++ src/renderer/mainRender.js
@@ -1,2 +1,14 @@
 // elsewhere in your code to send a message to the other renderers message handler
-window.electronMessagePort.postmessage('ping')
+// window.electronMessagePort.postmessage('ping')
+window.onmessage = (event) => {
+    console.log(event);
+    if (event.source === window && event.data === 'port') {
+        const [ port ] = event.ports
+        console.log(port);
+        port.postMessage('ping')
+        // 一旦我们有了这个端口，我们就可以直接与主进程通信
+        port.onmessage = (event) => {
+            console.log('from other process:', event.data)
+        }
+    }
+}
--- src/renderer/otherRender.js
+++ src/renderer/otherRender.js
@@ -1,2 +1,2 @@
 // elsewhere in your code to send a message to the other renderers message handler
-window.electronMessagePort.postmessage('pong')
+window.electronMessagePort.postmessage('pongtest')
